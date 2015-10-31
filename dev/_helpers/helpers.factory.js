f3e198304a03a9288fa51d832495d6b5c772e8ae
Edgar Zavala <ezavile@gmail.com>
2015-10-31 16:46:40 -0600

    add overflow boody

--- dev/_helpers/helpers.factory.js
+++ dev/_helpers/helpers.factory.js
@@ -9,6 +9,8 @@
 		var helperFactory = {};
 
 		helperFactory.popupClose = function () {
+			var body = angular.element(document).find('body');
+			body.removeClass('popup-on');
 			var popup = angular.element(document.querySelectorAll("[popup-close]"));
 			popup[0].remove();
 		};
